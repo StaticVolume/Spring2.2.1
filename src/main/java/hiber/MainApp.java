d0ec561fd4b3aaff0c89aa8b004ef1ff3b7612c6
Filipp <zodeck@mail.ru>
2022-10-02 15:57:21 +1000

    updated println(user)

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -22,12 +22,7 @@ public class MainApp {
 
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println("Car = " + user.getCar());
-         System.out.println();
+      System.out.println(user);
       }
       System.out.println("-------------------------------------------------------------------");
       System.out.println(userService.getUserByeCar(new Car("Mazda Demio", 102355)));
